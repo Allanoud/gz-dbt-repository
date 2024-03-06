dcd83599ce3d172be766091585a5fed68fa6b278
Alanoud Alrifai <alrifaialanoud@gmail.com>
2024-03-06 10:33:40 +0000

    s

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -8,5 +8,5 @@ SELECT
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
     s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__product")}} p 
+JOIN {{ref("stg_raw__product")}} p 
     USING (products_id)
